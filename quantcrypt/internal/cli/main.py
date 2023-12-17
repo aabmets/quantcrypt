9f696fb04319fcd1020e932846e627bf070eb422
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-12-17 23:17:55 +0200

    Project setup

---
+++ quantcrypt/internal/cli/main.py
@@ -0,0 +1,10 @@
+#
+#   MIT License
+#   
+#   Copyright (c) 2024, Mattias Aabmets
+#   
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#   
+#   SPDX-License-Identifier: MIT
+#
