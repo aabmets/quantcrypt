eca589fc24c08575d862824a8be04526c0f9fead
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-12-27 10:51:23 +0200

    Reworked find_abs_path and prepared compile script for GitHub Actions

--- scripts/compile.py
+++ scripts/compile.py
@@ -9,6 +9,7 @@
 #   SPDX-License-Identifier: MIT
 #
 import yaml
+import shutil
 import textwrap
 import platform
 from cffi import FFI
@@ -53,7 +54,7 @@ class BaseAlgorithm:
 					raise UnsupportedPlatformError(opsys)
 
 	def __init__(self, name: str, variant: Variant, subdir: str, ffi_cdefs: str):
-		pqclean = find_pqclean_dir()
+		pqclean = find_abs_path(rel_path='pqclean')
 		variant_path = pqclean / f"{subdir}/{name}/{variant.value}"
 		self.assert_platform_support(variant_path, variant)
 
@@ -136,19 +137,21 @@ def get_supported_algorithms(variant: Variant) -> Iterator[KemAlgorithm | DssAlg
 			continue
 
 
-@lru_cache(maxsize=1)
-def find_pqclean_dir() -> Path:
-	current_path = Path(__file__).parent.resolve()
+@lru_cache
+def find_abs_path(rel_path: str, from_file: str = __file__) -> Path:
+	current_path = Path(from_file).parent.resolve()
 	while current_path != current_path.parent:
-		path = current_path / 'pqclean'
-		if path.is_dir():
-			return path
+		search_path = current_path / rel_path
+		if search_path.exists():
+			return search_path
+		elif (current_path / ".git").exists():
+			break
 		current_path = current_path.parent
-	raise RuntimeError("Must find pqclean dir!")  # pragma: no cover
+	raise RuntimeError(f"Cannot find '{rel_path}' in repo!")
 
 
 def get_common_files() -> tuple[str, list[str]]:
-	path = find_pqclean_dir() / 'common'
+	path = find_abs_path(rel_path="pqclean/common")
 	return path.as_posix(), [
 		file.as_posix() for file in path.rglob("**/*")
 		if file.is_file() and file.name.endswith(".c")
@@ -178,7 +181,7 @@ def main():
 			ffi = FFI()
 			ffi.cdef(algo.ffi_cdefs)
 			ffi.set_source(
-				module_name=f"{opsys}.{variant.value}.{algo.name}",
+				module_name=f"bin.{opsys}.{variant.value}.{algo.name}",
 				source=f'#include "{algo.header_file}"',
 				sources=[*com_files, *algo.variant_files],
 				include_dirs=[com_dir],
@@ -188,9 +191,13 @@ def main():
 			)
 			ffi.compile(verbose=True)
 
-	source_dir = Path(__file__).with_name(opsys)
-	for file in source_dir.rglob("*.c"):
+	src_dir = Path(__file__).with_name("bin")
+	for file in src_dir.rglob("*.c"):
 		file.unlink(missing_ok=True)
+
+	dst_dir = find_abs_path("quantcrypt/internal")
+	shutil.rmtree(dst_dir / "bin")
+	shutil.move(src_dir, dst_dir)
 
 
 if __name__ == '__main__':
