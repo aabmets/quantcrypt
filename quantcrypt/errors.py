dc795ecd670cd656a1c7e1982a3d2814ef2bfa62
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-12-28 18:34:01 +0200

    Relocated errors.py to top-level

--- quantcrypt/errors.py
+++ quantcrypt/errors.py
@@ -1,18 +1,20 @@
 #
 #   MIT License
-#   
+#
 #   Copyright (c) 2024, Mattias Aabmets
-#   
+#
 #   The contents of this file are subject to the terms and conditions defined in the License.
 #   You may not use, modify, or distribute this file except in compliance with the License.
-#   
+#
 #   SPDX-License-Identifier: MIT
 #
 __all__ = [
 	"QuantCryptError",
 	"KeygenFailedError",
 	"EncapsFailedError",
-	"DecapsFailedError"
+	"DecapsFailedError",
+	"SignFailedError",
+	"VerifyFailedError"
 ]
 
 
@@ -24,14 +26,24 @@ class QuantCryptError(Exception):
 
 class KeygenFailedError(QuantCryptError):
 	def __init__(self):
-		super().__init__("Kupydo KEM keygen failed.")
+		super().__init__("QuantCrypt KEM/DSS keygen failed.")
 
 
 class EncapsFailedError(QuantCryptError):
 	def __init__(self):
-		super().__init__("Kupydo KEM encaps failed.")
+		super().__init__("QuantCrypt KEM encaps failed.")
 
 
 class DecapsFailedError(QuantCryptError):
 	def __init__(self):
-		super().__init__("Kupydo KEM decaps failed.")
+		super().__init__("QuantCrypt KEM decaps failed.")
+
+
+class SignFailedError(QuantCryptError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS sign failed.")
+
+
+class VerifyFailedError(QuantCryptError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS verify failed.")
