e7f2b3181326ddd71429abeb2cabbe96165149da
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-12-28 20:53:44 +0200

    Implemented Falcon and Sphincs DSS

--- quantcrypt/internal/crypto/dss.py
+++ quantcrypt/internal/crypto/dss.py
@@ -57,7 +57,7 @@ class BaseDSS(BasePQCAlgorithm, ABC):
 	def verify(self, public_key: bytes, message: bytes, signature: bytes, *, raises: bool = True) -> bool:
 		params = self.param_sizes
 		pk_anno = self._bytes_anno(equal_to=params.pk_size)
-		sig_anno = self._bytes_anno(min_size=params.sig_size)
+		sig_anno = self._bytes_anno(max_size=params.sig_size)
 		msg_anno = self._bytes_anno(min_size=1)
 
 		@validate_call(validate_return=True)
@@ -96,3 +96,78 @@ class DSS:
 		@property
 		def name(self) -> str:
 			return "dilithium5"
+
+	class Falcon(BaseDSS):
+		def __init__(self, variant: Variant = None):
+			"""
+			Initializes the Falcon instance with C extension binaries.
+			User is able to override which underlying binary is used for the
+			instance by providing a Variant enum for the variant parameter.
+
+			:param variant: Which binary to use underneath.
+				When variant is None *(auto-select mode)*, quantcrypt will
+				first try to import AVX2 binaries. If there are no AVX2 binaries
+				for the host platform, it will fall back to using CLEAN binaries.
+			:raises ImportError: When an unknown import error has occurred.
+			:raises ModuleNotFoundError: When variant is Variant.AVX2 *(manual-select mode)*
+				and quantcrypt cannot find AVX2 binaries for the current platform.
+			:raises SystemExit: When quantcrypt cannot find CLEAN binaries for
+				the current platform *(any-select mode)*. This is a fatal error
+				which requires the library to be reinstalled, because all platforms
+				should have CLEAN binaries available.
+			"""
+			super().__init__(variant)
+
+		@property
+		def name(self) -> str:
+			return "falcon-1024"
+
+	class FastSphincs(BaseDSS):
+		def __init__(self, variant: Variant = None):
+			"""
+			Initializes the FastSphincs instance with C extension binaries.
+			User is able to override which underlying binary is used for the
+			instance by providing a Variant enum for the variant parameter.
+
+			:param variant: Which binary to use underneath.
+				When variant is None *(auto-select mode)*, quantcrypt will
+				first try to import AVX2 binaries. If there are no AVX2 binaries
+				for the host platform, it will fall back to using CLEAN binaries.
+			:raises ImportError: When an unknown import error has occurred.
+			:raises ModuleNotFoundError: When variant is Variant.AVX2 *(manual-select mode)*
+				and quantcrypt cannot find AVX2 binaries for the current platform.
+			:raises SystemExit: When quantcrypt cannot find CLEAN binaries for
+				the current platform *(any-select mode)*. This is a fatal error
+				which requires the library to be reinstalled, because all platforms
+				should have CLEAN binaries available.
+			"""
+			super().__init__(variant)
+
+		@property
+		def name(self) -> str:
+			return "sphincs-shake-256f-simple"
+
+	class SmallSphincs(BaseDSS):
+		def __init__(self, variant: Variant = None):
+			"""
+			Initializes the SmallSphincs instance with C extension binaries.
+			User is able to override which underlying binary is used for the
+			instance by providing a Variant enum for the variant parameter.
+
+			:param variant: Which binary to use underneath.
+				When variant is None *(auto-select mode)*, quantcrypt will
+				first try to import AVX2 binaries. If there are no AVX2 binaries
+				for the host platform, it will fall back to using CLEAN binaries.
+			:raises ImportError: When an unknown import error has occurred.
+			:raises ModuleNotFoundError: When variant is Variant.AVX2 *(manual-select mode)*
+				and quantcrypt cannot find AVX2 binaries for the current platform.
+			:raises SystemExit: When quantcrypt cannot find CLEAN binaries for
+				the current platform *(any-select mode)*. This is a fatal error
+				which requires the library to be reinstalled, because all platforms
+				should have CLEAN binaries available.
+			"""
+			super().__init__(variant)
+
+		@property
+		def name(self) -> str:
+			return "sphincs-shake-256s-simple"
