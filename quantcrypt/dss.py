60037f5f5f331133509e79b5f590a01c0fc8a33f
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-01-12 16:37:12 +0200

    Made PQA exports explicit, updated file headers

--- quantcrypt/dss.py
+++ quantcrypt/dss.py
@@ -1,24 +1,24 @@
 #
 #   MIT License
-#
+#   
 #   Copyright (c) 2024, Mattias Aabmets
-#
+#   
 #   The contents of this file are subject to the terms and conditions defined in the License.
 #   You may not use, modify, or distribute this file except in compliance with the License.
-#
+#   
 #   SPDX-License-Identifier: MIT
 #
 from .internal.pqa.common import PQAVariant
-from .internal.pqa.errors import (
-	PQAError,
-	DSSKeygenFailedError,
-	DSSSignFailedError,
-	DSSVerifyFailedError
-)
 from .internal.pqa.dss import (
 	DSSParamSizes,
 	Dilithium,
 	Falcon,
 	FastSphincs,
 	SmallSphincs
+)
+from .internal.pqa.errors import (
+	PQAError,
+	DSSKeygenFailedError,
+	DSSSignFailedError,
+	DSSVerifyFailedError
 )
--- quantcrypt/errors.py
+++ quantcrypt/errors.py
@@ -1,14 +1,39 @@
 #
 #   MIT License
-#
+#   
 #   Copyright (c) 2024, Mattias Aabmets
-#
+#   
 #   The contents of this file are subject to the terms and conditions defined in the License.
 #   You may not use, modify, or distribute this file except in compliance with the License.
-#
+#   
 #   SPDX-License-Identifier: MIT
 #
-from .internal.errors import *
-from .internal.cipher.errors import *
-from .internal.kdf.errors import *
-from .internal.pqa.errors import *
+from .internal.errors import (
+	QuantCryptError,
+	InvalidUsageError,
+	InvalidArgsError
+)
+from .internal.cipher.errors import (
+	CipherError,
+	CipherStateError,
+	CipherVerifyError,
+	CipherChunkSizeError,
+	CipherPaddingError
+)
+from .internal.kdf.errors import (
+	KDFError,
+	KDFOutputLimitError,
+	KDFWeakPasswordError,
+	KDFVerificationError,
+	KDFInvalidHashError,
+	KDFHashingError
+)
+from .internal.pqa.errors import (
+	PQAError,
+	KEMKeygenFailedError,
+	KEMEncapsFailedError,
+	KEMDecapsFailedError,
+	DSSKeygenFailedError,
+	DSSSignFailedError,
+	DSSVerifyFailedError
+)
