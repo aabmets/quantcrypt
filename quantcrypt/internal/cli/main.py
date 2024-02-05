5272865be9db31119213abe43e4eb41b5df92a8d
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-02-05 10:51:46 +0200

    Restored --version and --info options to qclib CLI command

--- quantcrypt/internal/cli/main.py
+++ quantcrypt/internal/cli/main.py
@@ -8,32 +8,45 @@
 #   
 #   SPDX-License-Identifier: MIT
 #
-from typer import Typer
+from typing import Annotated
+from typer import Typer, Option
 from .info import PackageInfo
 from . import utils, console
 
 
 app = utils.add_commands(Typer(
-    name="qclib",
-    invoke_without_command=True,
-    no_args_is_help=True
+	name="qclib",
+	invoke_without_command=True,
+	no_args_is_help=True
 ))
 
 
-@app.command(name="version", help="Prints package version to the console and exits.")
-def version():
-    print(PackageInfo().Version)
+VersionAtd = Annotated[bool, Option(
+	'--version', '-v', show_default=False,
+	help="Prints package version to the console and exits."
+)]
+InfoAtd = Annotated[bool, Option(
+	'--info', '-i', show_default=False,
+	help="Prints project info to the console and exits."
+)]
 
 
-@app.command(name="info", help="Pretty-prints package info to the console and exits.")
-def info() -> None:
-    title_color = "[{}]".format("#ff5fff")
-    key_color = "[{}]".format("#87d7d7")
-    value_color = "[{}]".format("#ffd787")
+@app.callback()
+def main(version: VersionAtd = False, info: InfoAtd = False) -> None:
+	if version and info:
+		a, b = [f"[bold turquoise2]--{kw}[/]" for kw in ["version", "info"]]
+		console.raise_error(f"Cannot use {a} and {b} options simultaneously.")
+	elif version or info:
+		if version:
+			print(PackageInfo().Version)
+		else:
+			title_color = "[{}]".format("#ff5fff")
+			key_color = "[{}]".format("#87d7d7")
+			value_color = "[{}]".format("#ffd787")
 
-    console.styled_print(f"{title_color}Package Info:")
-    for k, v in PackageInfo().toDict().items():
-        k = f"{key_color}{k}"
-        v = f"{value_color}{v}"
-        console.styled_print(f"{2 * ' '}{k}: {v}")
-    console.styled_print('')
+			console.styled_print(f"{title_color}Package Info:")
+			for k, v in PackageInfo().toDict().items():
+				k = f"{key_color}{k}"
+				v = f"{value_color}{v}"
+				console.styled_print(f"{2 * ' '}{k}: {v}")
+			console.styled_print('')
--- tests/test_cli/test_main.py
+++ tests/test_cli/test_main.py
@@ -25,14 +25,15 @@ def fixture_project() -> DotMap:
 
 def test_main_version(project: DotMap):
 	runner = CliRunner()
-	result = runner.invoke(app, ["version"])
+	result = runner.invoke(app, ["--version"])
+
 	assert result.exit_code == 0
 	assert result.stdout.strip() == project.version
 
 
 def test_main_info(project: DotMap):
 	runner = CliRunner()
-	result = runner.invoke(app, ["info"])
+	result = runner.invoke(app, ["--info"])
 
 	assert result.exit_code == 0
 	assert project.name in result.stdout
@@ -43,3 +44,11 @@ def test_main_info(project: DotMap):
 
 	fn, ln, _ = project.authors[0].split(" ")
 	assert f"{fn} {ln}" in result.stdout
+
+
+def test_main_invalid_options():
+	runner = CliRunner()
+	result = runner.invoke(app, ["--version", "--info"])
+
+	assert result.exit_code == 1
+	assert "Cannot use --version and --info" in result.stdout
