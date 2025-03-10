8d5ec7b34746c89dd2c16aae5c5e1e1c2cab87e6
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-03-10 10:15:44 +0200

    Refactored PQA errors into main errors module

--- quantcrypt/internal/errors.py
+++ quantcrypt/internal/errors.py
@@ -8,10 +8,22 @@
 #   
 #   SPDX-License-Identifier: MIT
 #
+
+from typing import Literal
+
+
 __all__ = [
 	"QuantCryptError",
 	"InvalidUsageError",
-	"InvalidArgsError"
+	"InvalidArgsError",
+	"PQAError",
+	"PQAKeyArmorError",
+	"KEMKeygenFailedError",
+	"KEMEncapsFailedError",
+	"KEMDecapsFailedError",
+	"DSSKeygenFailedError",
+	"DSSSignFailedError",
+	"DSSVerifyFailedError"
 ]
 
 
@@ -27,3 +39,42 @@ class InvalidUsageError(QuantCryptError):
 class InvalidArgsError(QuantCryptError):
 	def __init__(self, message: str = None):
 		super().__init__(message or "Method received invalid arguments.")
+
+
+class PQAError(QuantCryptError):
+	"""Base class for all PQC errors."""
+
+
+class PQAKeyArmorError(PQAError):
+	def __init__(self, verb: Literal["armor", "dearmor"]):
+		super().__init__(f"QuantCrypt will not {verb} a corrupted key.")
+
+
+class KEMKeygenFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt KEM keygen failed.")
+
+
+class KEMEncapsFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt KEM encaps failed.")
+
+
+class KEMDecapsFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt KEM decaps failed.")
+
+
+class DSSKeygenFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS keygen failed.")
+
+
+class DSSSignFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS sign failed.")
+
+
+class DSSVerifyFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS verify failed.")
