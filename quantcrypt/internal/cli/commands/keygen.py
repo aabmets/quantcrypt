b79f60a6ee0363ffd1a848cbdef914e58f9f50bf
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-03-12 17:18:27 +0200

    Refactored CLI keygen command

--- quantcrypt/internal/cli/commands/keygen.py
+++ quantcrypt/internal/cli/commands/keygen.py
@@ -8,153 +8,105 @@
 #   
 #   SPDX-License-Identifier: MIT
 #
+
 import string
+from typer import Typer
 from typing import Type
-from typing import Annotated
-from typer import Typer, Option
-from quantcrypt.kem import Kyber
-from quantcrypt.dss import Dilithium, Falcon, FastSphincs, SmallSphincs
-from quantcrypt.internal.pqa.common import BasePQAlgorithm
-from .. import console, common as com
-from . import helpers as hlp
+from quantcrypt.internal import constants as const
+from quantcrypt.internal.cli import tools, console, annotations as ats
+from quantcrypt.internal.pqa import kem_algos
+from quantcrypt.internal.pqa import dss_algos
+from quantcrypt.internal.pqa.base_kem import BaseKEM
+from quantcrypt.internal.pqa.base_dss import BaseDSS
 
 
 app = Typer(
-	name="keygen", no_args_is_help=True, help=""
-	"Generates an ASCII armored keypair using a KEM or a DSS algorithm."
+    name="keygen", no_args_is_help=True,
+    help="Generates an ASCII armored keypair using a KEM or a DSS algorithm."
 )
 
 
-IdentifierAtd = Annotated[str, Option(
-	"--id", "-i", show_default=False, help=""
-	"Unique identifier to prepend to the names of the keyfiles, optional."
-)]
-DirectoryAtd = Annotated[str, Option(
-	"--dir", "-d", show_default=False, help=""
-	"Directory where to save the generated keypair, optional. "
-	"If the directory doesn't exist, it will be created with parents. "
-	"If not provided, the keys are saved into the Current Working Directory."
-)]
+def add_command(pqa_class: Type[BaseKEM | BaseDSS]) -> None:
+    spec: const.AlgoSpec = getattr(pqa_class, "_get_spec")()
+    @app.command(
+        name=pqa_class.__name__.replace('_', '').lower(),
+        help=f"[{spec.type.name}] Generates {spec.name} keys and writes them to disk."
+    )
+    def command_keygen(
+            identifier: ats.Identifier = None,
+            directory: ats.Directory = None,
+            dry_run: ats.DryRun = False,
+            overwrite: ats.Overwrite = False,
+            non_interactive: ats.NonInteractive = False
+    ) -> None:
+        args = [*locals().values(), pqa_class]
+        _interactive_flow(*args)
 
 
-@app.command(name="kyber")
-def command_kyber(
-		_id: IdentifierAtd = None,
-		_dir: DirectoryAtd = None,
-		_dry: com.DryRunAtd = False,
-		_ow: com.OverwriteAtd = False,
-		_ni: com.NonInteractiveAtd = False
-	) -> None:
-	"""[KEM] Generates Kyber keys and writes them to disk."""
-	_interactive_flow(_id, _dir, _dry, _ow, _ni, Kyber)
-
-
-@app.command(name="dilithium")
-def command_dilithium(
-		_id: IdentifierAtd = None,
-		_dir: DirectoryAtd = None,
-		_dry: com.DryRunAtd = False,
-		_ow: com.OverwriteAtd = False,
-		_ni: com.NonInteractiveAtd = False
-	) -> None:
-	"""[DSS] Generates Dilithium keys and writes them to disk."""
-	_interactive_flow(_id, _dir, _dry, _ow, _ni, Dilithium)
-
-
-@app.command(name="falcon")
-def command_falcon(
-		_id: IdentifierAtd = None,
-		_dir: DirectoryAtd = None,
-		_dry: com.DryRunAtd = False,
-		_ow: com.OverwriteAtd = False,
-		_ni: com.NonInteractiveAtd = False
-	) -> None:
-	"""[DSS] Generates Falcon keys and writes them to disk."""
-	_interactive_flow(_id, _dir, _dry, _ow, _ni, Falcon)
-
-
-@app.command(name="smallsphincs")
-def command_smallsphincs(
-		_id: IdentifierAtd = None,
-		_dir: DirectoryAtd = None,
-		_dry: com.DryRunAtd = False,
-		_ow: com.OverwriteAtd = False,
-		_ni: com.NonInteractiveAtd = False
-	) -> None:
-	"""[DSS] Generates SmallSphincs keys and writes them to disk."""
-	_interactive_flow(_id, _dir, _dry, _ow, _ni, SmallSphincs)
-
-
-@app.command(name="fastsphincs")
-def command_fastsphincs(
-		_id: IdentifierAtd = None,
-		_dir: DirectoryAtd = None,
-		_dry: com.DryRunAtd = False,
-		_ow: com.OverwriteAtd = False,
-		_ni: com.NonInteractiveAtd = False
-	) -> None:
-	"""[DSS] Generates FastSphincs keys and writes them to disk."""
-	_interactive_flow(_id, _dir, _dry, _ow, _ni, FastSphincs)
+for module in [kem_algos, dss_algos]:
+    for obj in vars(module).values():
+        if issubclass(obj, (BaseKEM, BaseDSS)):
+            add_command(obj)
 
 
 def _interactive_flow(
-		identifier: str | None,
-		directory: str | None,
-		dry_run: bool,
-		overwrite: bool,
-		non_interactive: bool,
-		algo_cls: Type[BasePQAlgorithm]
+        identifier: str | None,
+        directory: str | None,
+        dry_run: bool,
+        overwrite: bool,
+        non_interactive: bool,
+        pqa_class: Type[BaseKEM | BaseDSS]
 ) -> None:
-	console.notify_dry_run(dry_run)
+    console.notify_dry_run(dry_run)
 
-	prefix = ''
-	if identifier:
-		_validate_identifier(identifier)
-		prefix = f"{identifier}-"
+    prefix = ''
+    if identifier:
+        _validate_identifier(identifier)
+        prefix = f"{identifier}-"
 
-	algo_name = algo_cls.__name__.lower()
-	apk_name = f"{prefix}{algo_name}-pubkey.qc"
-	ask_name = f"{prefix}{algo_name}-seckey.qc"
+    algo_name = pqa_class.__name__.lower()
+    apk_name = f"{prefix}{algo_name}-pubkey.qc"
+    ask_name = f"{prefix}{algo_name}-seckey.qc"
 
-	target_dir = hlp.resolve_directory(directory)
-	apk_file = target_dir / apk_name
-	ask_file = target_dir / ask_name
+    target_dir = tools.resolve_directory(directory)
+    apk_file = target_dir / apk_name
+    ask_file = target_dir / ask_name
 
-	a, b = [f"[italic sky_blue2]{x.name}[/]" for x in [apk_file, ask_file]]
-	console.styled_print(
-		f"QuantCrypt is about to generate {a} and {b} files\n"
-		f"into the following directory: [italic tan]{target_dir}\n"
-	)
-	if not non_interactive:
-		console.ask_continue(exit_on_false=True)
+    a, b = [f"[italic sky_blue2]{x.name}[/]" for x in [apk_file, ask_file]]
+    console.styled_print(
+        f"QuantCrypt is about to generate {a} and {b} files\n"
+        f"into the following directory: [italic tan]{target_dir}\n"
+    )
+    if not non_interactive:
+        console.ask_continue(exit_on_false=True)
 
-	if apk_file.is_file() or ask_file.is_file():
-		console.ask_overwrite_files(
-			non_interactive, overwrite,
-			exit_on_false=True
-		)
+    if apk_file.is_file() or ask_file.is_file():
+        console.ask_overwrite_files(
+            non_interactive, overwrite,
+            exit_on_false=True
+        )
 
-	pqa = algo_cls()
-	public_key, secret_key = pqa.keygen()
-	apk = pqa.armor(public_key)
-	ask = pqa.armor(secret_key)
+    pqa = pqa_class()
+    public_key, secret_key = pqa.keygen()
+    apk = pqa.armor(public_key)
+    ask = pqa.armor(secret_key)
 
-	if dry_run:
-		console.styled_print("QuantCrypt would have created the following files:")
-		console.pretty_print([apk_file.as_posix(), ask_file.as_posix()])
-	else:
-		apk_file.write_text(apk)
-		ask_file.write_text(ask)
-		console.print_success()
+    if dry_run:
+        console.styled_print("QuantCrypt would have created the following files:")
+        console.pretty_print([apk_file.as_posix(), ask_file.as_posix()])
+    else:
+        apk_file.write_text(apk)
+        ask_file.write_text(ask)
+        console.print_success()
 
 
 def _validate_identifier(name_arg: str) -> None:
-	if len(name_arg) > 15:
-		console.raise_error("Unique identifier cannot be longer than 15 characters!")
-	allowed_chars = string.ascii_letters + string.digits
-	for char in name_arg:
-		if char not in allowed_chars:
-			console.raise_error(
-				"Only characters [[chartreuse3]a-z, A-Z,[/] "
-				"0-9] are allowed in the unique identifier!"
-			)
+    if len(name_arg) > 15:
+        console.raise_error("Unique identifier cannot be longer than 15 characters!")
+    allowed_chars = string.ascii_letters + string.digits
+    for char in name_arg:
+        if char not in allowed_chars:
+            console.raise_error(
+                "Only characters [[chartreuse3]a-z, A-Z,[/] "
+                "0-9] are allowed in the unique identifier!"
+            )
