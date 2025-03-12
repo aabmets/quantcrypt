5d771ec683803fb9295353f4ecd1246b2692f489
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-03-12 17:18:02 +0200

    Refactored CLI encrypt and decrypt commands

--- quantcrypt/internal/cli/commands/enc_dec.py
+++ quantcrypt/internal/cli/commands/enc_dec.py
@@ -8,126 +8,104 @@
 #
 #   SPDX-License-Identifier: MIT
 #
-from typing import Annotated
-from typer import Typer, Option
+
+from typer import Typer
 from quantcrypt.cipher import KryptonKEM
-from .. import console, common as com
-from . import helpers as hlp
+from quantcrypt.internal import constants as const
+from quantcrypt.internal.cli import tools, console, annotations as ats
 
 
 enc_app = Typer(
-	name="encrypt", invoke_without_command=True, no_args_is_help=True, help=""
-	"Uses an ASCII armored KEM public key to encrypt a file with the Krypton cipher."
+    name="encrypt", invoke_without_command=True, no_args_is_help=True,
+    help="Uses an ASCII armored KEM public key to encrypt a file with the Krypton cipher."
 )
 dec_app = Typer(
-	name="decrypt", invoke_without_command=True, no_args_is_help=True, help=""
-	"Uses an ASCII armored KEM secret key to decrypt a file with the Krypton cipher."
+    name="decrypt", invoke_without_command=True, no_args_is_help=True,
+    help="Uses an ASCII armored KEM secret key to decrypt a file with the Krypton cipher."
 )
-
-
-EncInFileAtd = Annotated[str, Option(
-	'--in-file', '-i', show_default=False, help=""
-	"Path to the plaintext data file, which will be encrypted with the Krypton cipher. "
-	"If the path is relative, it is evaluated from the Current Working Directory."
-)]
-EncOutFileAtd = Annotated[str, Option(
-	'--out-file', '-o', show_default=False, help=""
-	"Path to the output file where the encrypted data will be written to, optional. "
-	"Defaults to the Current Working Directory, using the data file name with the .kptn suffix."
-)]
-DecInFileAtd = Annotated[str, Option(
-	'--in-file', '-i', show_default=False, help=""
-	"Path to the ciphertext data file, which will be decrypted with the Krypton cipher. "
-	"If the path is relative, it is evaluated from the Current Working Directory."
-)]
-DecOutFileAtd = Annotated[str, Option(
-	'--out-file', '-o', show_default=False, help=""
-	"Path to the output file where the decrypted data will be written to, optional. "
-	"Defaults to the Current Working Directory, using the original filename of the "
-	"plaintext file that was stored into the ciphertext file."
-)]
 
 
 @enc_app.callback()
 def command_encrypt(
-		pk_file: com.PubKeyFileAtd,
-		in_file: EncInFileAtd,
-		out_file: EncOutFileAtd = None,
-		dry_run: com.DryRunAtd = False,
-		overwrite: com.OverwriteAtd = False,
-		non_interactive: com.NonInteractiveAtd = False
-	) -> None:
-	paths = hlp.process_paths(pk_file, in_file, out_file, '.kptn')
-	_interactive_flow(paths, dry_run, overwrite, non_interactive, 'PUBLIC')
+        pk_file: ats.PubKeyFile,
+        in_file: ats.EncInFile,
+        out_file: ats.EncOutFile = None,
+        dry_run: ats.DryRun = False,
+        overwrite: ats.Overwrite = False,
+        non_interactive: ats.NonInteractive = False
+) -> None:
+    paths = tools.process_paths(pk_file, in_file, out_file, const.KryptonFileSuffix)
+    _common_flow(paths, dry_run, overwrite, non_interactive, const.PQAKeyType.PUBLIC)
 
 
 @dec_app.callback()
 def command_decrypt(
-		sk_file: com.SecKeyFileAtd,
-		in_file: DecInFileAtd,
-		out_file: DecOutFileAtd = None,
-		dry_run: com.DryRunAtd = False,
-		overwrite: com.OverwriteAtd = False,
-		non_interactive: com.NonInteractiveAtd = False
-	) -> None:
-	paths = hlp.process_paths(sk_file, in_file, out_file, '.kptn')
-	_interactive_flow(paths, dry_run, overwrite, non_interactive, 'SECRET')
+        sk_file: ats.SecKeyFile,
+        in_file: ats.DecInFile,
+        out_file: ats.DecOutFile = None,
+        dry_run: ats.DryRun = False,
+        overwrite: ats.Overwrite = False,
+        non_interactive: ats.NonInteractive = False
+) -> None:
+    paths = tools.process_paths(sk_file, in_file, out_file, const.KryptonFileSuffix)
+    _common_flow(paths, dry_run, overwrite, non_interactive, const.PQAKeyType.SECRET)
 
 
-def _interactive_flow(
-		paths: hlp.CommandPaths,
-		dry_run: com.DryRunAtd,
-		overwrite: com.OverwriteAtd,
-		non_interactive: com.NonInteractiveAtd,
-		exp_kt: hlp.ExpKeyType
-	) -> None:
-	console.notify_dry_run(dry_run)
+def _common_flow(
+        paths: tools.CommandPaths,
+        dry_run: ats.DryRun,
+        overwrite: ats.Overwrite,
+        non_interactive: ats.NonInteractive,
+        key_type: const.PQAKeyType
+) -> None:
+    console.notify_dry_run(dry_run)
 
-	with paths.key_file.open('r') as file:
-		armored_key = file.read()
+    with paths.key_file.open('r') as file:
+        armored_key = file.read()
 
-	kem_class = hlp.determine_kem_class(armored_key, exp_kt)
+    armor_name = tools.validate_armored_key(armored_key, key_type, const.PQAType.KEM)
 
-	files = [paths.in_file, paths.key_file]
-	a, b = [f"[italic sky_blue2]{f.name}[/]" for f in files]
+    files = [paths.in_file, paths.key_file]
+    a, b = [f"[italic sky_blue2]{f.name}[/]" for f in files]
 
-	if exp_kt == 'PUBLIC':
-		console.styled_print(
-			f"QuantCrypt is about to encrypt the {a} plaintext file with the \n"
-			f"{b} KEM PK file into the following binary ciphertext file: \n"
-			f"[italic tan]{paths.out_file} \n"
-		)
-	else:
-		console.styled_print(
-			f"QuantCrypt is about to decrypt the {a} ciphertext file with \n"
-			f"the {b} KEM SK file into the following plaintext file: \n"
-			f"[italic tan]{paths.out_file} \n"
-		)
+    if key_type == const.PQAKeyType.PUBLIC:
+        console.styled_print(
+            f"QuantCrypt is about to encrypt the {a} plaintext file with the \n"
+            f"{b} KEM PK file into the following binary ciphertext file: \n"
+            f"[italic tan]{paths.out_file} \n"
+        )
+    else:
+        console.styled_print(
+            f"QuantCrypt is about to decrypt the {a} ciphertext file with \n"
+            f"the {b} KEM SK file into the following plaintext file: \n"
+            f"[italic tan]{paths.out_file} \n"
+        )
 
-	if not non_interactive:
-		console.ask_continue(exit_on_false=True)
+    if not non_interactive:
+        console.ask_continue(exit_on_false=True)
 
-	if paths.out_file.exists():
-		console.ask_overwrite_files(
-			non_interactive, overwrite,
-			exit_on_false=True
-		)
+    if paths.out_file.exists():
+        console.ask_overwrite_files(
+            non_interactive, overwrite,
+            exit_on_false=True
+        )
 
-	if dry_run:
-		console.styled_print("QuantCrypt would have created the following file:")
-		console.pretty_print([paths.out_file.as_posix()])
-	else:
-		krypton = KryptonKEM(kem_class)
-		if exp_kt == 'PUBLIC':
-			krypton.encrypt(
-				public_key=armored_key,
-				data_file=paths.in_file,
-				output_file=paths.out_file
-			)
-		else:
-			krypton.decrypt_to_file(
-				secret_key=armored_key,
-				encrypted_file=paths.in_file,
-				output_file=paths.out_file
-			)
-		console.print_success()
+    if dry_run:
+        console.styled_print("QuantCrypt would have created the following file:")
+        console.pretty_print([paths.out_file.as_posix()])
+    else:
+        kem_cls = tools.get_pqa_class(armor_name)
+        krypton = KryptonKEM(kem_cls)
+        if key_type == const.PQAKeyType.PUBLIC:
+            krypton.encrypt(
+                public_key=armored_key,
+                data_file=paths.in_file,
+                output_file=paths.out_file
+            )
+        else:
+            krypton.decrypt_to_file(
+                secret_key=armored_key,
+                encrypted_file=paths.in_file,
+                output_file=paths.out_file
+            )
+        console.print_success()
