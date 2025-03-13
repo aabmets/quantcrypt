539fc36d5723ead7faa6dbd4230849b4d785b792
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-03-14 01:25:58 +0200

    Refactored pytest conftest files

--- tests/test_cli/conftest.py
+++ tests/test_cli/conftest.py
@@ -8,103 +8,105 @@
 #
 #   SPDX-License-Identifier: MIT
 #
+
 import os
+import shutil
 import pytest
+import secrets
+import typing as t
 from pathlib import Path
-from dotmap import DotMap
-from typing import Callable
-from typer.testing import CliRunner
+from dataclasses import dataclass
+from contextlib import contextmanager
+from collections.abc import Callable
+from typer.testing import CliRunner, Result
 from quantcrypt.internal.cli.main import app
+from quantcrypt.internal.cli import commands as cmd
 
 
-@pytest.fixture(scope="package")
-def cli_message() -> DotMap:
-	return DotMap(
-		success="Operation successful!",
-		cancelled="Operation cancelled.",
-		ow_error="Must explicitly enable file overwriting"
-	)
+@dataclass(frozen=True)
+class CryptoFilePaths:
+    public_key_fp: str
+    secret_key_fp: str
+    ciphertext_fp: str
+    plaintext_fp: str
+    signature_fp: str
+    ptf_data: bytes
 
 
-@pytest.fixture(scope="function")
-def get_paths(tmp_path: Path) -> Callable:
-	def closure(algo_name: str) -> DotMap:
-		os.chdir(tmp_path)
-		runner = CliRunner()
-		result = runner.invoke(app, ["keygen", algo_name], input="y\n")
-		assert result.exit_code == 0
-
-		paths = DotMap(
-			pk_file=tmp_path / f"{algo_name}-pubkey.qc",
-			sk_file=tmp_path / f"{algo_name}-seckey.qc",
-			pt_file=tmp_path / "data_file.bin",
-			ct_file=tmp_path / "data_file.kptn",
-			sig_file=tmp_path / "data_file.sig"
-		)
-		with paths.pt_file.open("wb") as file:
-			file.write(os.urandom(1024))
-		return paths
-
-	return closure
+class CLIMessages:
+    ERROR = "QuantCrypt Error"
+    SUCCESS = "Operation successful"
+    CANCELLED = "Operation cancelled"
+    DRYRUN = "DRY RUN MODE"
 
 
-@pytest.fixture(scope="function")
-def success(get_paths: Callable, get_args: Callable, cli_message: DotMap) -> Callable:
-	def closure(algo_name: str, mode: str) -> None:
-		paths = get_paths(algo_name)
-		args = get_args(paths, mode)
-		runner = CliRunner()
-
-		result = runner.invoke(app, args, input="n\n")
-		assert result.exit_code == 0
-		assert cli_message.cancelled in result.stdout
-
-		result = runner.invoke(app, args, input="y\n")
-		assert result.exit_code == 0
-		assert cli_message.success in result.stdout
-
-	return closure
+CLIMessages = CLIMessages()
+ValidCommands = t.Literal["main", "info", "encrypt", "decrypt", "sign", "verify"]
 
 
-@pytest.fixture(scope="function")
-def dry_run(get_paths: Callable, get_args: Callable) -> Callable:
-	def closure(algo_name: str, mode: str) -> None:
-		paths = get_paths(algo_name)
-		args = get_args(paths, mode)
-		runner = CliRunner()
+@pytest.fixture(name="cfp_setup", scope="function")
+def fixture_cfp_setup(tmp_path: Path) -> Callable[..., t.ContextManager[CryptoFilePaths]]:
+    @contextmanager
+    def closure(
+            algorithm: str,
+            *,
+            touch: bool = False,
+            rmtree: bool = False
+    ) -> t.Generator[CryptoFilePaths, t.Any, None]:
+        sub_path = tmp_path / secrets.token_hex(16)
+        sub_path.mkdir(parents=True, exist_ok=True)
+        cfp_dict = dict(
+            public_key_fp=sub_path / f"{algorithm}-pubkey.qc",
+            secret_key_fp=sub_path / f"{algorithm}-seckey.qc",
+            ciphertext_fp=sub_path / "ciphertext.kptn",
+            plaintext_fp=sub_path / "plaintext.bin",
+            signature_fp=sub_path / "signature.sig",
+            ptf_data=os.urandom(1024)
+        )
+        if touch:
+            for path in cfp_dict.values():
+                path.touch(exist_ok=False)
+        cfp = CryptoFilePaths(**{
+            k: v.as_posix() if isinstance(v, Path) else v
+            for k, v in cfp_dict.items()
+        })
+        with open(cfp.plaintext_fp, "wb") as file:
+            file.write(cfp.ptf_data)
+        cwd = os.getcwd()
+        os.chdir(sub_path)
+        yield cfp
+        os.chdir(cwd)
+        if rmtree:
+            shutil.rmtree(sub_path, ignore_errors=True)
+    return closure
 
-		result = runner.invoke(app, args + ['-D'], input="y\n")
-		assert result.exit_code == 0
-		assert "DRY RUN MODE" in result.stdout
 
-	return closure
-
-
-@pytest.fixture(scope="function")
-def overwrite(get_paths: Callable, get_args: Callable, cli_message: DotMap) -> Callable:
-	def closure(algo_name: str, mode: str) -> None:
-		paths = get_paths(algo_name)
-		args = get_args(paths, mode)
-		runner = CliRunner()
-
-		result = runner.invoke(app, args, input="y\n")
-		assert result.exit_code == 0
-		assert cli_message.success in result.stdout
-
-		result = runner.invoke(app, args, input="y\nn\n")
-		assert result.exit_code == 0
-		assert cli_message.cancelled in result.stdout
-
-		result = runner.invoke(app, args, input="y\ny\n")
-		assert result.exit_code == 0
-		assert cli_message.success in result.stdout
-
-		result = runner.invoke(app, args + ['-N'])
-		assert result.exit_code == 1
-		assert cli_message.ow_error in result.stdout
-
-		result = runner.invoke(app, args + ['-N', '-W'])
-		assert result.exit_code == 0
-		assert cli_message.success in result.stdout
-
-	return closure
+@pytest.fixture(name="cli_runner", scope="function")
+def fixture_cli_runner() -> Callable[..., Result]:
+    def closure(
+            command: ValidCommands = "main",
+            options: list[str] = None,
+            user_input: str = None,
+            expected_stdout: str = None,
+            debug: bool = False
+    ) -> Result:
+        runner = CliRunner()
+        _app = dict(
+            main=app,
+            info=cmd.info_app,
+            keygen=cmd.keygen_app,
+            encrypt=cmd.enc_app,
+            decrypt=cmd.dec_app,
+            sign=cmd.sign_app,
+            verify=cmd.verify_app,
+        )[command]
+        result = runner.invoke(_app, options, input=user_input)
+        if debug:
+            print(result.output)
+        expected_exit_code = 1
+        if expected_stdout != CLIMessages.ERROR:
+            expected_exit_code = 0
+        assert result.exit_code == expected_exit_code
+        assert (expected_stdout or '') in result.stdout
+        return result
+    return closure
