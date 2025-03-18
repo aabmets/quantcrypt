dcf35c03eb1c8d67915848bfa63fceb2fa6a26f0
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-03-18 12:24:01 +0200

    Fixed compiler args for macos GHA runner

--- quantcrypt/internal/compiler.py
+++ quantcrypt/internal/compiler.py
@@ -123,11 +123,14 @@ class Target:
             if arch in const.AMDArches:
                 for flag in self.required_flags:
                     extra_flags.append(f"-m{flag.lower()}")
-            elif arch in const.ARMArches and opsys == "linux":
-                extra_flag = "-march=armv8.5-a"
-                for flag in self.required_flags:
-                    extra_flag += f"+{flag.lower()}"
-                extra_flags.append(extra_flag)
+            elif arch in const.ARMArches:
+                if opsys == "darwin":
+                    extra_flags.append("-mfloat-abi=hard")
+                else:
+                    extra_flag = "-march=armv8.5-a"
+                    for flag in self.required_flags:
+                        extra_flag += f"+{flag.lower()}"
+                    extra_flags.append(extra_flag)
             return [
                 "-s", "-fdata-sections", "-ffunction-sections",
                 "-O3", "-flto", "-std=c99", *extra_flags,
